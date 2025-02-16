31a9d4dd7c751d852e8952de88f2cc75a980197f
Felix Packard <felix@rigr.gg>
2025-02-16 14:16:36 +0000

    benchmark algorithms in a group to facilitate easier comparison

--- src/benches/benchmarks.rs
+++ src/benches/benchmarks.rs
@@ -1,34 +1,31 @@
-use sorting_algorithms::algorithms;
-
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rand::{Rng, SeedableRng};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("algorithms");
+
     macro_rules! bench {
-        ($algorithm:ident, $size:literal) => {
-            c.bench_function(
-                format!("{} {}", stringify!($algorithm), $size).as_str(),
-                |b| {
-                    b.iter(|| {
-                        algorithms::$algorithm::sort(black_box(
-                            &mut sorting_algorithms::utils::rand_data($size),
-                        ))
-                    })
-                },
-            );
+        ($algorithm:ident, $size:ident) => {
+            group.bench_function(format!("{} {}", stringify!($algorithm), $size), |b| {
+                let rng = rand::rngs::SmallRng::seed_from_u64(0);
+                b.iter_batched_ref(
+                    || -> Vec<i32> {
+                        rng.clone()
+                            .sample_iter(rand::distr::StandardUniform)
+                            .take($size)
+                            .collect()
+                    },
+                    |v| sorting_algorithms::algorithms::$algorithm::sort(v),
+                    BatchSize::SmallInput,
+                )
+            });
         };
     }
 
-    macro_rules! bench_all {
-        ($algorithm:ident) => {
-            bench!($algorithm, 10);
-            bench!($algorithm, 10_000);
-            bench!($algorithm, 100_000);
-            bench!($algorithm, 1_000_000);
-        };
+    for i in [10, 10_000, 100_000, 1_000_000] {
+        bench!(std, i);
+        bench!(quicksort, i);
     }
-
-    bench_all!(std);
-    bench_all!(quicksort);
 }
 
 criterion_group!(benches, criterion_benchmark);
