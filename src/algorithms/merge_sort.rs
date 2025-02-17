8f6cbe797199b788eb270bc8dbcba08cd7e562e2
Felix Packard <felix@rigr.gg>
2025-02-17 05:45:33 +0000

    add merge sort

--- src/algorithms/merge_sort.rs
+++ src/algorithms/merge_sort.rs
@@ -1,46 +1,51 @@
-pub fn sort<T: Ord + Copy>(v: &mut [T]) {
+pub fn sort<T: Ord + Copy + std::fmt::Display + std::fmt::Debug>(v: &mut [T]) {
     let n = v.len();
     if n <= 1 {
         return;
     }
 
     let mut width = 1;
+    let mut buffer = vec![v[0]; n];
+
     while width < n {
-        let mut left = 0;
-        while left < n {
-            let right = std::cmp::min(left + width * 2, n);
-            merge(&mut v[left..right]);
-            left += width * 2;
+        for left in (0..n - 1).step_by(2 * width) {
+            let mid = std::cmp::min(left + width, n);
+            let right = std::cmp::min(left + 2 * width, n);
+
+            let left_slice = &v[left..mid];
+            let right_slice = &v[mid..right];
+
+            merge(left_slice, right_slice, &mut buffer[left..right]);
+
+            v[left..right].copy_from_slice(&buffer[left..right]);
         }
+
         width *= 2;
     }
 }
 
-fn merge<T: Ord + Copy>(v: &mut [T]) {
-    let mid = v.len() / 2;
-    let left = v[..mid].to_vec();
-    let right = v[mid..].to_vec();
-
+fn merge<T: Ord + Copy + std::fmt::Display>(left_half: &[T], right_half: &[T], v: &mut [T]) {
     let (mut i, mut j, mut k) = (0, 0, 0);
-    while i < left.len() && j < right.len() {
-        if left[i] <= right[j] {
-            v[k] = left[i];
+
+    while i < left_half.len() && j < right_half.len() {
+        if left_half[i] < right_half[j] {
+            v[k] = left_half[i];
             i += 1;
         } else {
-            v[k] = right[j];
+            v[k] = right_half[j];
             j += 1;
         }
         k += 1;
     }
 
-    while i < left.len() {
-        v[k] = left[i];
+    while i < left_half.len() {
+        v[k] = left_half[i];
         i += 1;
         k += 1;
     }
 
-    while j < right.len() {
-        v[k] = right[j];
+    while j < right_half.len() {
+        v[k] = right_half[j];
         j += 1;
         k += 1;
     }
